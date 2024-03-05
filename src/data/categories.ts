cea527b332a1dab4ad4eb5d14409397acab04f04
dawapaljor <dawapaljor@tibetaction.net>
2024-03-05 14:55:18 +0530

    chaged

--- src/data/categories.ts
+++
@@ -1,9 +0,0 @@
-// List of categories for blog posts
-export const CATEGORIES = [
-	'Wordpress Tricks',
-	'Linux',
-	'GitHub',
-	'report',
-	'Category 4',
-	'Category 5'
-] as const
--- src/utils/index.ts
+++
@@ -1,4 +0,0 @@
-export { sluglify, unsluglify } from './sluglify'
-export { cn } from './cn'
-export { getCategories, getPosts } from './post'
-export { remarkReadingTime } from './readTime'
