845c2717833d5d5acf5783d08df36dd59de4bdd1
dawapaljor <dawapaljor@tibetaction.net>
2024-04-01 16:56:36 +0530

    added token

--- tina/config.ts
+++ tina/config.ts
@@ -15,6 +15,7 @@ export default defineConfig({
  clientId: "ee5040e8-2364-4d34-952e-f17c44fe3435",
   // Get this from tina.io
   //token: process.env.TINA_TOKEN,
+token: "fa48ce3c17f99c499918bd1d7b1d592b847dcc6c",
 
   build: {
     outputFolder: "admin",
