0ccba4af7ebb42757c89d2c973e50ea14b867551
dawapaljor <dawapaljor@tibetaction.net>
2024-04-01 16:53:13 +0530

    added new posts

--- tina/config.ts
+++ tina/config.ts
@@ -11,9 +11,10 @@ export default defineConfig({
   branch,
 
   // Get this from tina.io
-  clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+ // clientId: process.env.NEXT_PUBLIC_TINA_CLIENT_ID,
+ clientId: "ee5040e8-2364-4d34-952e-f17c44fe3435"
   // Get this from tina.io
-  token: process.env.TINA_TOKEN,
+  //token: process.env.TINA_TOKEN,
 
   build: {
     outputFolder: "admin",
