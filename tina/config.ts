f93bc180c1041a826b01edce4d4ec22e13bc016e
Dawa Paljor <dawapaljor@tibetaction.net>
2024-07-12 15:11:43 +0530

    added

--- tina/config.ts
+++ tina/config.ts
@@ -16,10 +16,14 @@ export default defineConfig({
     publicFolder: "public",
   },
   media: {
-    tina: {
-      mediaRoot: "assets",
-      publicFolder: "public",
-    },
+   // tina: {
+   //   mediaRoot: "assets",
+   //   publicFolder: "public",
+   // },
+   loadCustomStore: async () => {
+         const pack = await import("next-tinacms-cloudinary");
+         return pack.TinaCloudCloudinaryMediaStore;
+       },
   },
   // See docs on content modeling for more info on how to setup new content models: https://tina.io/docs/schema/
   schema: {
